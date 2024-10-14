1b7a2b1b2f88911d7caf3fdb8cbe5765c5e06706
jamesw98 <xeekatar@gmail.com>
2024-10-13 23:09:58 -0400

    comments and other cleanup

--- src/GHSFL.Points/Utils.cs
+++ src/GHSFL.Points/Utils.cs
@@ -1,13 +1,18 @@
 ﻿using System.Data;
-using System.Net;
 using System.Text.Json;
 using GHSFL.Points.Models;
 
 namespace GHSFL.Points;
 
-public class Utils
+public static class Utils
 {
-    public static async Task<SeasonInfo> GetPointsFromFile(HttpClient http)
+    /// <summary>
+    /// Gets data from the info file.
+    /// </summary>
+    /// <param name="http">HttpClient to use.</param>
+    /// <returns>The season info object used to populate the table.</returns>
+    /// <exception cref="DataException">If we ran into any issues getting the data.</exception>
+    public static async Task<SeasonInfo> GetDataFromFile(HttpClient http)
     {
         try
         {
