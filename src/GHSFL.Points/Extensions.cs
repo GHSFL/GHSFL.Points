bdb9372e122ff002658eb95f4df0e6e48537e1c4
jamesw98 <xeekatar@gmail.com>
2024-10-21 19:10:47 -0400

    added school view

---
+++ src/GHSFL.Points/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using GHSFL.Points.Models;
+
+namespace GHSFL.Points;
+
+public static class Extensions
+{
+    public static void AddOrUpdate(this List<SchoolInfo> schoolInfo, string club, int points)
+    {
+        var entry = schoolInfo.FirstOrDefault(x => x.Club == club);
+        if (entry is null)
+        {
+            schoolInfo.Add(new SchoolInfo
+            {
+                Club = club,
+                Points = points
+            });
+        }
+        else
+        {
+            entry.Points += points;
+        }
+    }
+}
---
+++ src/GHSFL.Points/Models/SchoolInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace GHSFL.Points.Models;
+
+public class SchoolInfo
+{
+    public string Club { get; set; }
+    public int Points { get; set; }
+}
